9b53ea2d06b4cc2e9f0841d1d10b3ea0f3590fda
Thorsten Tarrach <thorsten@atomia.com>
2011-12-14 13:19:19 +0100

    copy msi to root folder

--- buildproject.bat
+++ buildproject.bat
@@ -2,5 +2,6 @@ call c:\Python26\Scripts\cxfreeze.bat atomia_manager\atomia.py --target-dir dist
 IF %ERRORLEVEL% NEQ 0 GOTO lexit
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WindowsInstaller/WindowsInstaller.sln /p:Configuration=Release
 IF %ERRORLEVEL% NEQ 0 GOTO lexit
+copy /y WindowsInstaller\bin\Release\*.msi .
 :lexit
 exit %ERRORLEVEL%
