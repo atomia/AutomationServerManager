7139298b6b03ee6293b3de999cd8320b70c3951b
vukasin <vukasin@atomia.com>
2012-01-24 11:07:56 +0100

    Setup now builds jsonpath package too.

--- platform_package.sh
+++ platform_package.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 if [ -x "/usr/bin/dpkg" ]; then
+	# atomia manager package
 	./exec_fpm.sh -n atomia-manager -s python -t deb ./setup.py
 	python setup.py bdist_wininst
+	
+	# jsonpath package
+	./exec_fpm.sh -n python-jsonpath -s python -t deb jsonpath
 	mv dist/*.exe .
 elif [ -x "/bin/rpm" ]; then
 	./exec_fpm.sh -n atomia-manager -s python -t rpm ./setup.py
+	
+	./exec_fpm.sh -n python-jsonpath -s python -t rpm jsonpath
 else
 	echo "unknown platform, exiting"
 	exit 1
