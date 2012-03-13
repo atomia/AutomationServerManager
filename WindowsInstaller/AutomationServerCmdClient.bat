bd89a28bfad99d93ac9cddb61c942d3247fce3b3
Thorsten Tarrach <thorsten@atomia.com>
2012-03-13 13:53:01 +0100

    Added a better console so this is more fun for people to use
    Change-Id: I6894591a0db3b3e371124fb93a7032ed0b02592c

--- WindowsInstaller/AutomationServerCmdClient.bat
+++ WindowsInstaller/AutomationServerCmdClient.bat
@@ -1,3 +1,4 @@
 @set path=%~dp0;%~dp0Shell;%path%
-@echo "Atomia Automation Server command line client"
-@cmd /k atomia.exe -h
+@echo Atomia Automation Server command line client
+@echo.
+@echo Type atomia -h for help on the command line parameters
