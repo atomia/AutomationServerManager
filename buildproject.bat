68c4bcc0d21dd0292542487d52d7f345034808fc
Thorsten Tarrach <thorsten@atomia.com>
2012-04-03 14:40:48 +0200

    64 bit version build now
    Change-Id: I1528240985fb7fbb8f6b22d1ddad97713f4f3e60

--- buildproject.bat
+++ buildproject.bat
@@ -1,8 +1,10 @@
 del *.msi
-call c:\Python26\Scripts\cxfreeze.bat atomia_manager\atomia.py --target-dir dist
+call c:\Python27-64\Scripts\cxfreeze.bat atomia_manager\atomia.py --target-dir dist
 IF %ERRORLEVEL% NEQ 0 GOTO lexit
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WindowsInstaller/WindowsInstaller.sln /p:Configuration=Release
 IF %ERRORLEVEL% NEQ 0 GOTO lexit
 copy /y WindowsInstaller\bin\Release\*.msi .
+IF %ERRORLEVEL% EQU 0 GOTO mexit
 :lexit
 exit %ERRORLEVEL%
+:mexit
