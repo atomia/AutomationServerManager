2b08b225a51185cd033457c747fe769d1d1de59e
Dusan Milenkovic <dusan@atomia.com>
2012-06-28 12:09:38 +0200

    Removed obsolete package dependency. Updated version number.
    Change-Id: I0f624830893dafcbf003ccd8f9a5e53595da6d4d

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url = "https://github.com/atomia/AutomationServerManager",
     packages=find_packages(),
     long_description=read('README.md'),
-	install_requires=['argparse >= 1.1', 'jsonpath >= 0.53', 'ntplib >= 0.1.9'],
+	install_requires=['argparse >= 1.1', 'jsonpath >= 0.53'],
 	setup_requires=['setuptools'],
     classifiers=[
         "Development Status :: 4 - Beta",
