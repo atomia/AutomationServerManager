6aa64ab1e3a1c8d68c8ef60eea7cab6b047cc997
Jimmy Bergman <jimmy@atomia.com>
2014-10-03 09:34:15 +0200

    No need to build exe on linux

--- platform_package.sh
+++ platform_package.sh
@@ -7,9 +7,6 @@ cat "atomia-client/setup.py.input" | sed -e "s/%VERSION/$version_from_file/g" > 
 if [ -x "/usr/bin/dpkg" ]; then
 	# atomia manager package
 	./exec_fpm.sh -n atomia-manager -s python -t deb ./setup.py
-	#if ! python setup.py bdist_wininst; then
-	#	exit 1
-	#fi
 	
 	# jsonpath package
 	if ! ./exec_fpm.sh -n python-jsonpath -s python -t deb jsonpath; then
@@ -19,7 +16,6 @@ if [ -x "/usr/bin/dpkg" ]; then
 	if ! sh ./exec_fpm.sh -n python-atomia-client -s python -t deb atomia-client/setup.py; then
 		exit 1
 	fi
-	mv dist/*.exe .
 elif [ -x "/bin/rpm" ]; then
 	if ! ./exec_fpm.sh -n atomia-manager -s python -t rpm ./setup.py; then
 		exit 1
